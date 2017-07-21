bbb1edc9db665bdad5ad8f646c1b35023a716908
VISHALY216626 <svishaly123@gmail.com>
2017-07-21 16:50:19 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringToInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		  String n=in.nextLine();
+		  int x=Integer.valueOf(n);
+		  System.out.println(x);
+	}
+
+}
