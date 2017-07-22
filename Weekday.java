459ef382126e91fa5e6a2690ec2d7eb193bd1de9
VISHALY216626 <svishaly123@gmail.com>
2017-07-22 10:32:35 +0530

    Create Weekday.java

---
+++ Weekday.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Weekday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Scanner in=new Scanner(System.in);
+			String s=in.nextLine();
+			if(s=="Sunday")
+			{
+				System.out.println("false");
+			}
+			else
+			{
+				System.out.println("true");
+			}
+	}
+
+	}
