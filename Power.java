281392d4c39c943cb2ca8742f1453e2da5276542
VISHALY216626 <svishaly123@gmail.com>
2017-07-22 10:43:01 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		int s=in.nextInt();
+		double a;
+		a=Math.pow(s,2);
+		System.out.println(a);
+		
+	}
+
+}
